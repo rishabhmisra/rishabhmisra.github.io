6320107b5514ceb82eff32188f31e30f829c5663
Rishabh Misra <rishabhmisra1994@gmail.com>
2022-06-16 13:20:34 -0700

    Update scholar_stats.rb

--- _plugins/scholar_stats.rb
+++ _plugins/scholar_stats.rb
@@ -3,8 +3,7 @@ require 'nokogiri'
 
 module Jekyll
   class ScholarStats < Generator
-    # Replace `SCHOLAR_ID` with your own Google Scholar ID
-    SCHOLAR_ID = 'XXXXXXXXXX'.freeze
+    SCHOLAR_ID = 'EN3OcMsAAAAJ'.freeze
     SCHOLAR_URL = 'http://scholar.google.com/citations?hl=en&user='.freeze
     def generate(site)
       doc = Nokogiri::HTML(URI.parse(SCHOLAR_URL + SCHOLAR_ID).open)
